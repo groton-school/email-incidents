75a60fde4f61e83fb5d96d34f505911ac0f06603
Seth Battis <sbattis@groton.org>
2023-02-09 18:59:56 +0000

    Prep for real updates

--- iiq-emails-source.js
+++ iiq-emails-source.js
@@ -1,14 +1,18 @@
+// TODO detect whether affected page is IIQ (only run on viable pages)
+// TODO do not re-add on repeat clicks
+// TODO escape title text for safer email addresses
+// TODO second invocation should remove links
 $('.link-view-ticket[href*="/tickets/"]').each((i, a) => {
   const guid = /tickets\/(.*)/.exec(a.href)[1];
-  const link = $('<a href="#">Copy Email Address</a>').appendTo($(a).parent().parent());
-  console.log(link);
+  const link = $('<a class="iiq-email link" href="#">Copy Email Address</a>').appendTo($(a).parent().parent());
   const info = $(a).closest('.ticket-info-cell');
   const number = $(info).find('.ticket-number').text();
+  const title = a.innerText;
   $(link).click(e => {
     e.stopPropagation();
     e.preventDefault();
-    navigator.clipboard.writeText('"' + a.innerText + ' (Ticket #' + number + ')" <' + guid + '@groton.incidentiq.com>');
-    const message = $('body').append('<p id="' + guid + '-copied" style="position: absolute; top: 1em; right: 1em; padding: 1em; background: lightgoldenrodyellow; color: darkgoldenrod; border-radius: 0.5em; border: solid 1px goldenrod; z-index:100000;">Email address for ' + a.innerText + ' (Ticket #' + number + ') copied to clipboard.</p>');
+    navigator.clipboard.writeText('"' + title + ' (Ticket #' + number + ')" <' + guid + '@groton.incidentiq.com>');
+    const message = $('body').append('<p id="' + guid + '-copied" style="position: absolute; top: 1em; right: 1em; padding: 1em; background: lightgoldenrodyellow; color: darkgoldenrod; border-radius: 0.5em; border: solid 1px goldenrod; z-index:100000;">Email address for ' + title + ' (Ticket #' + number + ') copied to clipboard.</p>');
     setTimeout(() => {
       $('#' + guid + '-copied').remove();
     }, 1000);
