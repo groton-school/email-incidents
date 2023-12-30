8bc131aeb704b47d41e60db65e4ec360760b19d6
Seth Battis <sbattis@groton.org>
2023-12-29 21:51:37 -0500

    simplify

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,38 +1,20 @@
 (() => {
   const host = location.host;
 
-  function Await(result, callback) {
-    if (result.$$state.status !== 1) {
-      setTimeout(() => Await(result, callback));
-    } else {
-      callback(result.$$state.value.data.Item);
-    }
-  }
-
   async function addEmailLink(cell) {
     const a = cell.querySelector('.link-view-ticket[href*="/tickets/"]');
     const guid = /tickets\/(.*)/.exec(a.href)[1];
-    const link = document.createElement('a');
-    link.classList.add('iiq-email', 'link');
-    link.innerText = 'Send Email';
-    link.href = '#';
+    const div = document.createElement('div');
+    div.innerHTML = `<a href="#" class="iiq-email link" target="_blank">Send Email</a>`;
+    const link = div.firstChild;
     cell.appendChild(link);
 
-    const title = a.innerText // https://stackoverflow.com/a/14890774
-      .replace(/'\b/g, '\u2018') // Opening singles
-      .replace(/\b'/g, '\u2019') // Closing singles
-      .replace(/"\b/g, '\u201c') // Opening doubles
-      .replace(/\b"/g, '\u201d') // Closing doubles
-      .replace(/--/g, '\u2014') // em-dashes
-      .replace(/\b\u2018\b/g, "'"); // And things like "it's" back to normal.;
-
-    Await(iiq.WebApi.GetTicket(guid), (ticket) => {
-      link.href = `mailto:${
-        ticket.For.Email
-      }?cc=${`"${title} (Ticket #${ticket.TicketNumber})" <${guid}@${host}>`}&body=${`${ticket.For.FirstName} --`}`;
+    iiq.WebApi.GetTicket(guid).then(({ data: { Item: ticket } }) => {
+      link.href = `mailto:${ticket.For.Email}?cc=${encodeURIComponent(
+        `"${ticket.Subject} (Ticket # ${ticket.TicketNumber})" <${ticket.TicketId}@${host}>`
+      )}&body=${encodeURIComponent(`${ticket.For.FirstName} --`)}`;
       link.addEventListener('click', (e) => {
         e.stopPropagation();
-        console.log(e.target.href);
       });
     });
   }
@@ -43,7 +25,7 @@
         if (node.querySelectorAll) {
           Array.from(
             node.querySelectorAll(
-              '.ticket-info-cell:not(:has(a.iiq-email)):has(.link-view-ticket[href*="/tickets/"])'
+              '.ticket-info-cell:not(:has(a.iiq-email.link)):has(.link-view-ticket[href*="/tickets/"])'
             )
           ).forEach(addEmailLink);
         }
